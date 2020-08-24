36f82dbf0b898b909d3a8695bd1bb47e24deb5e8
Alesey Kosenkov <starko.web@gmail.com>
2020-08-24 10:43:17 +0300

    main

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from fight import *
+from fight import fp, sp, fight
+from conf import STOP_PROGRAM
 
-stop_program = False    
 
-while stop_program != True:
-    if fight(fp, sp) == True or fight(sp, fp) == True:
-        stop_program = True
+while STOP_PROGRAM != True:
+  if fight(fp, sp) == True or fight(sp, fp) == True:
+    STOP_PROGRAM = True
     
 if fp.get_health() > sp.get_health():
-    print('\n' + '[' + fp.get_nickname() + '] victory with ' + str(fp.get_health()) + ' health')
+  print('\n' + '[' + fp.get_nickname() + '] victory with ' 
+  + str(fp.get_health()) + ' health')
 else:
-    print('\n' + '['  + sp.get_nickname() + '] victory with ' + str(sp.get_health()) + ' health')
+  print('\n' + '['  + sp.get_nickname() + '] victory with ' 
+  + str(sp.get_health()) + ' health')
